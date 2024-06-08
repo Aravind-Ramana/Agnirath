816622cfede6d1fcb430c5783d19b97627f176c5
Aravind Ramana <aravindramanav@gmail.com>
2024-06-08 11:33:24 +0530

    stratv7:control_stop_analysis

--- strategy/strategy_versions/strategy_v7/state.py
+++ strategy/strategy_versions/strategy_v7/state.py
@@ -5,7 +5,7 @@ import pandas as pd
 ModelMethod = "COBYLA"
 InitialGuessVelocity =25
 
-RaceStartTime = 9 * 3600  # 8:00 am
+RaceStartTime = 8 * 3600  # 8:00 am
 RaceEndTime = (17) * 3600  # 5:00 pm
 
 Day=1
@@ -13,7 +13,7 @@ Day=1
 # BatteryLevel
 BatteryLevelWayPoints = [1, 0.808, 0.488, 0.408, 0.28, 0.2]
 # Route DF
-DF_WayPoints = [0, 110, 220, 330, 440, 550]
+DF_WayPoints = [0, 102, 207, 301, 415, 520]
 
 InitialBatteryCapacity = None
 FinalBatteryCapacity = None
@@ -24,6 +24,6 @@ def set_day_state(day_no):
     Day = day_no
     InitialBatteryCapacity = config.BatteryCapacity * BatteryLevelWayPoints[Day-1] # Wh
     FinalBatteryCapacity = config.BatteryCapacity * BatteryLevelWayPoints[Day]  # Wh
-    route_df = pd.read_csv("./data/processed_route_data.csv").iloc[DF_WayPoints[Day-1]: DF_WayPoints[Day]]
+    route_df = pd.read_csv("processed_route_data.csv").iloc[DF_WayPoints[Day-1]: DF_WayPoints[Day]]
 
 set_day_state(Day)
